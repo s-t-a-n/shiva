134c6b98ca253241b791834c44f2072c9484c2f5
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2021-04-17 14:28:58 +0200

    Bootstrapping (#1)
    * README.md: skeleton
    
    * Github action workflows
    
    * Gitignore
    
    * CMakeLists.txt: skeleton
    
    * minimake.sh
    
    * tests folder: skeleton
    
    * src folder: skeleton

---
+++ minimake.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+project=cpp-shiva
+build_dir=build
+
+if [ $# -eq 1 ] && [ "$1" == "test" ]; then
+	# compile and test
+	cmake -S . -B $build_dir -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Debug && ( cd $build_dir && make ) \
+	&& ( cd $build_dir && ctest --output-on-failure ./) \
+	&& echo -e "Tests ran: \e[92mok\e[39m." \
+	|| { echo -e "Tests ran: \e[91mNOPE\e[39m."; false; }
+elif [ $# -eq 1 ] && [ "$1" == "testOpt" ]; then
+	# compile and test with optimisation
+	cmake -S . -B $build_dir -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo && ( cd $build_dir && make ) \
+	&& ( cd $build_dir && ctest --output-on-failure ./) \
+	&& echo -e "Tests ran: \e[92mok\e[39m." \
+	|| { echo -e "Tests ran: \e[91mNOPE\e[39m."; false; }
+elif [ $# -eq 1 ] && [ "$1" == "debug" ]; then
+	# compile with debugging flags
+	cmake -S . -B $build_dir -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Debug && ( cd $build_dir && make ) \
+	&& cp $build_dir/src/lib${project}.a ./ \
+	&& echo -e "You can include \e[92mlib${project}.a\e[39m in your library." \
+	|| { echo -e "Compilation ran: \e[91mNOPE\e[39m."; false; }
+elif [ $# -eq 1 ] && [ "$1" == "clean" ]; then
+	# clean all files which are specified in .gitignore 
+	git clean -d -f -X
+elif [ $# -eq 0 ] || [[ $# -eq 1  &&  "$1" == "release" ]]; then
+	# compile and copy ${project} binary and lib to root folder
+	cmake -S . -B $build_dir -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo && ( cd $build_dir && make ) \
+	&& cp $build_dir/src/lib${project}.a ./ \
+	&& echo -e "You can include \e[92mlib${project}.a\e[39m in your library." \
+	|| { echo -e "Compilation ran: \e[91mNOPE\e[39m."; false; }
+else
+	cat<<-EOF
+	usage:
+	  $0         -> compile for release
+	  $0 release -> compile for release
+	  $0 debug   -> compile for debugging
+	  $0 test    -> compile for debugging and test
+	  $0 testOpt -> compile for debugging (with optimization flags) and test
+	  $0 clean   -> remove build files 
+	EOF
+fi
+
