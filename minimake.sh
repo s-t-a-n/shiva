3c8343e898bd9c97060523ac9f483eebbbea0218
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-04-30 12:58:37 +0200

    CmakeLists.txt, minimake.sh: cpp-shiva -> shiva

--- minimake.sh
+++ minimake.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-project=cpp-shiva
+project=shiva
 build_dir=build
+targets=("${project}-debugger")
 
 if [ $# -eq 1 ] && [ "$1" == "test" ]; then
 	# compile and test
@@ -18,8 +19,7 @@ elif [ $# -eq 1 ] && [ "$1" == "testOpt" ]; then
 elif [ $# -eq 1 ] && [ "$1" == "debug" ]; then
 	# compile with debugging flags
 	cmake -S . -B $build_dir -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Debug && ( cd $build_dir && make ) \
-	&& cp $build_dir/src/lib${project}.a ./ \
-	&& echo -e "You can include \e[92mlib${project}.a\e[39m in your library." \
+	&& echo -e "You can add target \e[92m${targets[*]}\e[39m to your library." \
 	|| { echo -e "Compilation ran: \e[91mNOPE\e[39m."; false; }
 elif [ $# -eq 1 ] && [ "$1" == "clean" ]; then
 	# clean all files which are specified in .gitignore 
@@ -27,8 +27,7 @@ elif [ $# -eq 1 ] && [ "$1" == "clean" ]; then
 elif [ $# -eq 0 ] || [[ $# -eq 1  &&  "$1" == "release" ]]; then
 	# compile and copy ${project} binary and lib to root folder
 	cmake -S . -B $build_dir -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo && ( cd $build_dir && make ) \
-	&& cp $build_dir/src/lib${project}.a ./ \
-	&& echo -e "You can include \e[92mlib${project}.a\e[39m in your library." \
+	&& echo -e "You can add target \e[92m${targets[*]}\e[39m to your library." \
 	|| { echo -e "Compilation ran: \e[91mNOPE\e[39m."; false; }
 else
 	cat<<-EOF
